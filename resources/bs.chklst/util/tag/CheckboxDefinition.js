3138557016fea76374d5096056ef6b52076c1cda
miriam schlindwein <schlindwein@hallowelt.com>
2022-08-19 08:59:52 +0200

    Add droplets
    Split tag in two separate tags: one for checkbox, one for list
    
    ERM29391
    
    [4.x]
    
    Change-Id: If5cfa26672d019d5462be839670f7ed525ba657d
    (cherry picked from commit ae5dfc0e0a0c52a58fd5901079bd9169cdb83f06)

---
+++ resources/bs.chklst/util/tag/CheckboxDefinition.js
@@ -0,0 +1,36 @@
+bs.util.registerNamespace( 'bs.chklst.util.tag' );
+bs.chklst.util.tag.CheckboxDefinition = function BsVecUtilTagCheckboxDefinition() {
+	bs.chklst.util.tag.CheckboxDefinition.super.call( this );
+};
+
+OO.inheritClass( bs.chklst.util.tag.CheckboxDefinition, bs.chklst.util.tag.ChecklistDefinition );
+
+bs.chklst.util.tag.CheckboxDefinition.prototype.getCfg = function() {
+	var cfg = bs.chklst.util.tag.CheckboxDefinition.super.prototype.getCfg.call( this );
+	return $.extend( cfg, {
+		classname : 'Checkbox',
+		name: 'checkbox',
+		tagname: 'bs:checkbox',
+		descriptionMsg: 'bs-checkbox-tag-checkbox-desc',
+		menuItemMsg: 'bs-checkbox-ve-checkboxinspector-title',
+		toolGroup: 'object',
+		tabbed: false,
+		tabs: [{
+			name: 'check',
+			labelMsg: 'bs-checklist-tag-checklist-tab-checkbox',
+			value: 'check'
+		}],
+		attributes: [{
+			name: 'checked',
+			labelMsg: 'bs-checklist-ve-checklistinspector-cb-checked',
+			helpMsg: 'bs-checklist-tag-checklist-attr-checked-help',
+			type: 'toggle',
+			default: 'false',
+			tab: 'check'
+		}]
+	});
+};
+
+bs.vec.registerTagDefinition(
+	new bs.chklst.util.tag.CheckboxDefinition()
+);
